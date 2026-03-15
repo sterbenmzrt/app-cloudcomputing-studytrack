3e5bacbd9cdeff3a98cdadc85990a6d27bb9522e
ahmadrivaldhi <ahmad.rivaldhi@sekolahmu.co.id>
2026-03-15 19:54:23 +0700

    feat: Add build script to generate `firebase-config.js` from environment variables.

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cat > firebase-config.js << EOF
+const firebaseConfig = {
+  apiKey:            "${FIREBASE_API_KEY}",
+  authDomain:        "${FIREBASE_AUTH_DOMAIN}",
+  databaseURL:       "${FIREBASE_DATABASE_URL}",
+  projectId:         "${FIREBASE_PROJECT_ID}",
+  storageBucket:     "${FIREBASE_STORAGE_BUCKET}",
+  messagingSenderId: "${FIREBASE_MESSAGING_SENDER_ID}",
+  appId:             "${FIREBASE_APP_ID}"
+};
+EOF
+
+echo "firebase-config.js generated successfully"
